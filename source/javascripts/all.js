81c09f28e52d4fba172755df586c3c913cbdfa4f
Safiye Sepetci <leyla.kapi@lab2023.com>
2014-09-04 11:45:55 +0300

    edit js file

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,18 +1,3 @@
-/* ===========================================================
- * jquery-onepage-scroll.js v1.3.1
- * ===========================================================
- * Copyright 2013 Pete Rojwongsuriya.
- * http://www.thepetedesign.com
- *
- * Create an Apple-like website that let user scroll
- * one page at a time
- *
- * Credit: Eike Send for the awesome swipe event
- * https://github.com/peachananr/onepage-scroll
- *
- * License: GPL v3
- *
- * ========================================================== */
 
 !function($){
 
@@ -30,9 +15,6 @@
     direction : 'vertical'
 	};
 
-	/*------------------------------------------------*/
-	/*  Credit: Eike Send for the awesome swipe event */
-	/*------------------------------------------------*/
 
 	$.fn.swipeEvents = function() {
       return this.each(function() {
